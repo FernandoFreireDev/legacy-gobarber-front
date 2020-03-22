1c36a6f53f2a96654d4ce1a94e57844e528d6608
Fernando Freire <fernando.freire@bemobi.com.br>
2020-03-22 10:11:05 -0300

    configurando reactotron

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import History from './services/history';
 
+import './config/ReactotronConfig';
+
 function App() {
   return (
     <Router history={History}>
